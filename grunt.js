d05fdc63f01ce00d1921d1f4bc097961e7b0981f
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2012-12-02 22:59:01 +0100

    add dependencies, set up files and base process

--- grunt.js
+++ grunt.js
@@ -2,40 +2,18 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    test: {
-      files: ['test/**/*.js']
-    },
-    lint: {
-      files: ['grunt.js', 'tasks/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'default'
-    },
-    jshint: {
-      options: {
-		smarttag : true,
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        es5: true
-      },
-      globals: {}
-    }
+		jsdoc : {
+			doc: {
+				src: ['src/*.js', 'test/*.js'],
+				dest: 'tmp'
+			}
+		}
   });
 
   // Load local tasks.
   grunt.loadTasks('tasks');
 
   // Default task.
-  grunt.registerTask('default', 'lint test');
+  grunt.registerTask('default', 'jsdoc');
 
 };
--- tasks/jsdoc-plugin.js
+++ tasks/jsdoc-plugin.js
@@ -9,20 +9,24 @@
 module.exports = function(grunt) {
 	'use strict';
 
-	grunt.registerTask('jsdoc', 'Generate Javascript Doc ', function() {
-		grunt.log.write(grunt.helper('jsdoc'));
-		
+	var util = require('util');
+
+	grunt.registerMultiTask('jsdoc', 'Generate Javascript Doc ', function() {
+		grunt.log.write("Helper : " + grunt.helper('jsdoc'));
 		var exec	= require('child_process').exec,
 			done	= this.async();
+
+		grunt.log.write("\nData : " + util.inspect(grunt.task.current));
 	
-		exec("pwd", function (error, stdout, stderr) {
-			grunt.log.write('stdout: ' + stdout);
-			grunt.log.write('stderr: ' + stderr);
+		//./node_modules/jsdoc/jsdoc -r -d tmp/ ../removablearea/src
+		exec("node_modules/jsdoc/jsdoc -h", function (error, stdout, stderr) {
+			grunt.log.write('stdout: ' + stdout + '\n');
+			grunt.log.write('stderr: ' + stderr + '\n');
 			if (error) {
 				grunt.log.write('exec error: ' + error);
 			}
 		});
-		grunt.log.write("\ntest");
+		grunt.log.write("\n" + process.cwd() + "\n");
 	});
 
   grunt.registerHelper('jsdoc', function() {
