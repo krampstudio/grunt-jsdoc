bd8507bbaf463bd8be00c12d2466f10515cd5aef
Jerry Sievert <code@legitimatesounding.com>
2013-12-31 04:54:19 -0800

    update for jsdoc3.3.0

--- tasks/jsdoc-plugin.js
+++ tasks/jsdoc-plugin.js
@@ -33,7 +33,7 @@ module.exports = function jsDocTask(grunt) {
 			done			= grunt.task.current.async(),
 			srcs			= grunt.task.current.filesSrc,
 			jsDocPath		= grunt.task.current.data.jsdoc,
-			jsDocNpmPath	= 'node_modules/jsdoc/jsdoc',
+			jsDocNpmPath	= 'node_modules/jsdoc/jsdoc.js',
 			timeout			= 60000,	//todo implement and move in options
 			cliFlags = ['recurse', 'private', 'lenient', 'explain', 'help', 'version', 'test', 'verbose', 'nocolor', 'template', 'configure', 'destination', 'encoding', 'tutorials', 'match', 'query'],
 			jsDoc;
