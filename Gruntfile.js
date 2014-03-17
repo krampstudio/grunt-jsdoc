2060c98d66a1c56d429bc020db0884d35642f4b2
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-03-17 19:03:06 +0100

    Gruntfile: Fix indent, all using spaces, and some whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 module.exports = function(grunt) {
-	'use strict';
+  'use strict';
 
   // Project configuration.
   grunt.initConfig({
-	clean : ['doc'],
+    clean : ['doc'],
     jsdoc : {
-		basic: {
-			src: ['tasks/**.js', 'tasks/lib/*.js'],
-			options: {
-				destination: 'doc/basic'
-			}
-		},
-        docstrap : {
-            src: ['tasks/**.js', 'tasks/lib/*.js', 'README.md'],
-            options:{
-				destination: 'doc/docstrap',
-                template: "node_modules/ink-docstrap/template",
-                configure: "node_modules/ink-docstrap/template/jsdoc.conf.json"
-			}
+      basic : {
+        src : ['tasks/**.js', 'tasks/lib/*.js'],
+        options : {
+          destination: 'doc/basic'
         }
-	},
-	nodeunit : {
-        unit : ['test/jsdoc-plugin_test.js'],
-        basic : ['test/jsdoc-basic_test.js'],
-        docstrap : ['test/jsdoc-docstrap_test.js']
-	},
-	jshint : {
-		files : ['Gruntfile.js', 'tasks/*.js', 'tasks/lib/*.js', 'test/*.js'],
-		options: {
-			node : true,
-			smarttabs : true
-		}
-	}
+      },
+      docstrap : {
+        src : ['tasks/**.js', 'tasks/lib/*.js', 'README.md'],
+        options : {
+          destination : 'doc/docstrap',
+            template : "node_modules/ink-docstrap/template",
+            configure : "node_modules/ink-docstrap/template/jsdoc.conf.json"
+        }
+      }
+    },
+    nodeunit : {
+      unit : ['test/jsdoc-plugin_test.js'],
+      basic : ['test/jsdoc-basic_test.js'],
+      docstrap : ['test/jsdoc-docstrap_test.js']
+    },
+    jshint : {
+      files : ['Gruntfile.js', 'tasks/*.js', 'tasks/lib/*.js', 'test/*.js'],
+      options: {
+        node : true,
+        smarttabs : true
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
