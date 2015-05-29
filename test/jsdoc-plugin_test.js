0dd79a69f89a5e849e2914f454c8c54fa2d663b6
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-05-29 22:13:09 +0200

    remove trailing spaces

--- test/jsdoc-plugin_test.js
+++ test/jsdoc-plugin_test.js
@@ -1,9 +1,9 @@
 /**
- * @fileoverview NodeUnit test 
+ * @fileoverview NodeUnit test
  * @copyright Bertrand Chevrier 2012
  * @author Bertrand Chevrier <chevrier.bertrand@gmail.com>
  * @license MIT
- * 
+ *
  * @module test/jsdoc-plugin_test
  */
 
@@ -15,7 +15,7 @@ var grunt = require('grunt');
 
 
 /**
- * This function enables you to extract 
+ * This function enables you to extract
  * the declared arguments from a function.
  * @param {Function} fn - the function to extract the arguments for
  * @returns {Array} the list of arguments
@@ -32,13 +32,13 @@ var extractArgs = function(fn){
 
 /**
  * NodeUnit group of test that check the jsdoc Grunt task.
- * 
+ *
  * @see https://github.com/caolan/nodeunit/
- * 
+ *
  * @class JsdocTest
  */
 exports.JsdocTest = {
-	
+
 	/**
 	 * Set up the test by load the tasks/jsdoc-plugin module
 	 * @memberOf JsdocTest
@@ -57,16 +57,16 @@ exports.JsdocTest = {
 	 * @param {Object} test - the node unit test context
 	 */
 	'taskCheck' : function(test){
-		'use strict';	
-	
+		'use strict';
+
 		test.notStrictEqual(this.jsdocTask, undefined, 'the jsdoc task should be set up');
-		test.equal(typeof this.jsdocTask, 'function', 'the task must be a function');	
-		
+		test.equal(typeof this.jsdocTask, 'function', 'the task must be a function');
+
 		var taskArgs = extractArgs(this.jsdocTask);
 		test.ok(taskArgs.length > 0 && taskArgs[0] === 'grunt', 'the task must declare the grunt context as 1st parameter');
-		
+
 		test.done();
-	}, 
+	},
 
 	/**
 	 * Do some check on the exec library
@@ -74,16 +74,16 @@ exports.JsdocTest = {
 	 * @param {Object} test - the node unit test context
 	 */
 	'execCheck' : function(test){
-		'use strict';	
+		'use strict';
 
 		var exec  = require('../tasks/lib/exec');
 
 		test.notStrictEqual(exec, undefined, 'the exec lib should be required');
-		test.equal(typeof exec, 'object', 'exec is an object');	
+		test.equal(typeof exec, 'object', 'exec is an object');
 
-		test.equal(typeof exec.buildSpawned, 'function', 'exec must have a buildSpawned method');	
-		test.equal(typeof exec.lookup, 'function', 'exec must have a lookup method');	
-		
+		test.equal(typeof exec.buildSpawned, 'function', 'exec must have a buildSpawned method');
+		test.equal(typeof exec.lookup, 'function', 'exec must have a lookup method');
+
 		test.done();
 	}
 };
